aa3b5afb3492713ea3e0892a6a2408ee96deef6a
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2021-03-17 22:31:40 +0100

    Update CheckBoard
    CheckBoard is now either the check of the LoRa Module and the reading/displaying of the all sensor values.

--- CheckBoard-I-Nucleo-LRWAN/CheckBoard-I-Nucleo-LRWAN.ino
+++ CheckBoard-I-Nucleo-LRWAN/CheckBoard-I-Nucleo-LRWAN.ino
@@ -1,18 +1,47 @@
 #include "LoRaWANNode.h"
+#include <HTS221Sensor.h>
+#include <LPS22HBSensor.h>
+#include <LSM303AGR_ACC_Sensor.h>
+#include <LSM303AGR_MAG_Sensor.h>
 
+#define I2C2_SCL    D15//PB10
+#define I2C2_SDA    D14//PB11
 HardwareSerial SerialLora(D0, D1); // D0(Rx) D1(TX)
 HardwareSerial Serial1(PA10, PA9);
+TwoWire *dev_i2c;
 
-void setup()
-{
+// Components.
+HTS221Sensor  *HumTemp;
+LPS22HBSensor *PressTemp;
+LSM303AGR_ACC_Sensor *Acc;
+LSM303AGR_MAG_Sensor *Mag;
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial1.begin(115200);
+
+  // Initialize I2C bus.
+  dev_i2c = new TwoWire(I2C2_SDA, I2C2_SCL);
+  dev_i2c->begin();
+
+  // Initlialize components.
+  HumTemp = new HTS221Sensor (dev_i2c);
+  HumTemp->Enable();
+  PressTemp = new LPS22HBSensor(dev_i2c);
+  PressTemp->Enable();
+  Acc = new LSM303AGR_ACC_Sensor(dev_i2c);
+  Acc->Enable();
+  Acc->EnableTemperatureSensor();
+  Mag = new LSM303AGR_MAG_Sensor(dev_i2c);
+  Mag->Enable();
+
   Serial1.println("\r\n\r\n\r\n");
   Serial1.println("########################################");
   Serial1.println("######## LoRaWAN Training Session ######");
-  Serial1.println("########     Check Board          ######\r\n");
+  Serial1.println("#########     WELCOME !!!!!!!   ########\r\n");
 
   Serial1.println(" Connecting to the LoRa Module ...\r\n");
-  // Enable the USI module and set the radio band.
   while (!loraNode.begin(&SerialLora, LORA_BAND_EU_868)) {
     Serial1.println("Lora module not ready");
     delay(1000);
@@ -22,10 +51,54 @@ void setup()
   str = " Unique DevEUI: 0x";
   loraNode.getDevEUI(&str);
   Serial1.println(str);
+  Serial1.println("\r\n  Let's start !\r\n");
 
 }
 
-void loop()
-{
-  //empty loop
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2500);
+
+  // Read humidity, pressure, temperature from LPS22HB, temperature from HTS221
+  float pressure, humidity, temperatureFromHTS221, temperatureFromLPS22HB, temperatureFromLSM303AGR;
+  int32_t accelerometer[3];
+  int32_t magnetometer[3];
+  HumTemp->GetHumidity(&humidity);
+  HumTemp->GetTemperature(&temperatureFromHTS221);
+  PressTemp->GetPressure(&pressure);
+  PressTemp->GetTemperature(&temperatureFromLPS22HB);
+  Acc->GetAxes(accelerometer);
+  Acc->GetTemperature(&temperatureFromLSM303AGR);
+  Mag->GetAxes(magnetometer);
+  
+  Serial1.print(" Humidity : ");
+  Serial1.print(humidity, 2);
+  Serial1.print("%   |   Temperature[HTS221] : ");
+  Serial1.print(temperatureFromHTS221, 2);
+  Serial1.print("C   |   Presure : ");
+  Serial1.print(pressure, 2);
+  Serial1.print("hPa   |   Temperature[LPS22HB] : ");
+  Serial1.print(temperatureFromLPS22HB, 2);
+  Serial1.println("C");
+
+  Serial1.print(" Accelerometer [mg]     : ");
+  Serial1.print(accelerometer[0]);
+  Serial1.print("(x) ");
+  Serial1.print(accelerometer[1]);
+  Serial1.print("(y) ");
+  Serial1.print(accelerometer[2]);
+  Serial1.println("(z)");
+  Serial1.print(" Magnetometer [mGauss]  : ");
+  Serial1.print(magnetometer[0]);
+  Serial1.print("(x) ");
+  Serial1.print(magnetometer[1]);
+  Serial1.print("(y) ");
+  Serial1.print(magnetometer[2]);
+  Serial1.println("(z)");
+  Serial1.print(" Temperature[LSM303AGR] : ");
+  Serial1.print(temperatureFromLSM303AGR, 2);
+  Serial1.println("C");
+  Serial1.println("");
 }
