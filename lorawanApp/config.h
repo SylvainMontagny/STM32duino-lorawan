727fa3f78f0b95317d871867425977343ea16516
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2021-12-22 17:48:27 +0100

    New functions : activationABP / activationOTAA / showConfig

--- lorawanApp/config.h
+++ lorawanApp/config.h
@@ -1,8 +1,35 @@
-#define ACTIVATION_MODE     ABP
+/*---------------------------------------------------------------------------------------------------------------
+ ACTIVATION_MODE     |    ABP or OTAA    |    # Selection of Activation Method                                  |
+ SEND_BY_PUSH_BUTTON |   true or false   |    # Sending method (Time or Push Button)                            |  
+ FRAME_DELAY         |    Time in ms     |    # Time between 2 frames (Minimum 7000)                            |
+ DATA_RATE           |   Number [0;5]    |    # 5=SF7BW125 ->  0=SF12BW125                                      |
+ ADAPTIVE_DR         |  true or false    |    # Enable Aaptive Data Rate (if true)                              |
+ CONFIRMED           |  true or false    |    # Frame Confirmed (if true) OR Frame Unconfirmed (if false)       |
+ PORT                |   Number [0;199]  |    # Application Port number                                         |
+ ENABLE_HUMIDITY     |  true or false    |    # Enable Humidity Sensor (if true)                                |
+ CAYENNE_LPP         |  true or false    |    # Enable all sensor and use the CAYENNE LPP format (if true)      |
+ LOW_POWER           |  true or false    |    # Enable Low Power mode between two frames (if true)              |
+---------------------------------------------------------------------------------------------------------------*/
+
+
+#define ACTIVATION_MODE     OTAA
 #define SEND_BY_PUSH_BUTTON true
 #define FRAME_DELAY         10000
 #define DATA_RATE           5
 #define ADAPTIVE_DR         false
 #define CONFIRMED           false
-#define PORT                1 
-#define LOW_POWER           true
+#define PORT                2 
+#define ENABLE_HUMIDITY     false
+#define CAYENNE_LPP         false
+#define LOW_POWER           false
+
+
+// Configuration for ABP Activation Mode
+const char devAddr[] = "0198F02F";
+const char nwkSKey[] = "6FE8DBC22F848A16EE7E29D73AF9492C";
+const char appSKey[] = "51360BCB7E7926A127B97A9AD19A4C61";
+
+
+// Configuration for OTAA Activation Mode
+const char appKey[] = "BFFAEFC231CA5697CF9B2AE413C48C46";
+const char appEUI[] = "0000000000000000";
--- lorawanApp/lorawanApp.ino
+++ lorawanApp/lorawanApp.ino
@@ -2,34 +2,62 @@
 #include "STM32LowPower.h"
 #include "config.h"
 
+#if ENABLE_HUMIDITY == true || CAYENNE_LPP == true
+      #include <HTS221Sensor.h>
+      #include <LPS22HBSensor.h>
+      #include <LSM303AGR_ACC_Sensor.h>
+      #include <LSM303AGR_MAG_Sensor.h>
+      #include <CayenneLPP.h>
+      #define I2C2_SCL    D15//PB10
+      #define I2C2_SDA    D14//PB11
+      TwoWire *dev_i2c;
+      HTS221Sensor  *HumTemp;
+      LPS22HBSensor *PressTemp;
+      LSM303AGR_ACC_Sensor *Acc;
+      LSM303AGR_MAG_Sensor *Mag;
+      // Read humidity, pressure, temperature from LPS22HB, temperature from HTS221
+      float pressure, humidity, temperatureFromHTS221, temperatureFromLPS22HB, temperatureFromLSM303AGR;
+      int32_t accelerometer[3];
+      int32_t magnetometer[3];
+      int8_t humidityLora =0;
+      CayenneLPP lpp(51);
+#endif
 
-char frameTx[] = "Hello";
-String str;
 
 HardwareSerial SerialLora(D0, D1); // D0(Rx) D1(TX)
 HardwareSerial Serial1(PA10, PA9);
 
-#if ACTIVATION_MODE == ABP
-const char devAddr[] = "0198F02F";
-const char nwkSKey[] = "6FE8DBC22F848A16EE7E29D73AF9492C";
-const char appSKey[] = "51360BCB7E7926A127B97A9AD19A4C61";
-#endif
-
-#if ACTIVATION_MODE == OTAA
-const char appKey[] = "";
-const char appEUI[] = "";
-#endif
+char frameTx[] = "Hello";
+String str;
 
 
 void setup()
 {  
   Serial1.begin(115200);
 #if LOW_POWER == true
-  LowPower.begin();
+    LowPower.begin();
 #endif
+
+#if ENABLE_HUMIDITY == true || CAYENNE_LPP == true
+      lpp.reset();  // Initialise CAYENNE 
+      dev_i2c = new TwoWire(I2C2_SDA, I2C2_SCL);
+      dev_i2c->begin();
+      HumTemp = new HTS221Sensor (dev_i2c);
+      HumTemp->Enable();
+      PressTemp = new LPS22HBSensor(dev_i2c);
+      PressTemp->Enable();
+      Acc = new LSM303AGR_ACC_Sensor(dev_i2c);
+      Acc->Enable();
+      Acc->EnableTemperatureSensor();
+      Mag = new LSM303AGR_MAG_Sensor(dev_i2c);
+      Mag->Enable();
+#endif
+  
   pinMode(PUSHBUTTON, INPUT);
+  showConfig();
   infoBeforeActivation();
-  activation();
+  if (ACTIVATION_MODE == ABP) activationABP();
+  if (ACTIVATION_MODE == OTAA) activationOTAA();
   infoAfterActivation();
 }
 
@@ -37,10 +65,30 @@ void loop()
 {
   if( SEND_BY_PUSH_BUTTON == 1)   while(digitalRead(PUSHBUTTON)); // Attente Push Button pour envoyer
   else                            delay((FRAME_DELAY<7000)?0:FRAME_DELAY-7000);  // Attente FRAME_DELAY pour envoyer
-  Serial1.print(" Sending Text : \"");Serial1.print(frameTx);Serial1.print("\"");
-  if(CONFIRMED)   Serial1.print(" Uplink CONFIRMED on PORT ");
-  else            Serial1.print(" Uplink UNCONFIRMED on PORT ");
-  Serial1.println(PORT);
+
+#if ENABLE_HUMIDITY == true || CAYENNE_LPP == true  
+      HumTemp->GetHumidity(&humidity);
+      HumTemp->GetTemperature(&temperatureFromHTS221);
+      PressTemp->GetPressure(&pressure);
+      PressTemp->GetTemperature(&temperatureFromLPS22HB);
+      Acc->GetAxes(accelerometer);
+      Acc->GetTemperature(&temperatureFromLSM303AGR);
+      Mag->GetAxes(magnetometer);
+      lpp.reset();
+      
+      // Channel 1 : Data from HTS221 (Humidity sensor)
+      lpp.addRelativeHumidity(1, humidity);
+      lpp.addTemperature(1, temperatureFromHTS221);
+      
+      // Channel 2 : Data from LPS22HB (Pressure sensor)
+      lpp.addBarometricPressure(2, pressure);
+      lpp.addTemperature(2, temperatureFromLPS22HB);
+      
+      // Channel 3 : Data from LSM303AGR (accelerometer/magnetometer/gyroscope)
+      lpp.addAccelerometer(3, (float)accelerometer[0]/1000, (float)accelerometer[1]/1000, (float)accelerometer[2]/1000);
+      lpp.addTemperature(2, temperatureFromLSM303AGR);
+#endif  
+  
   transmit();
   receive();
   
@@ -80,17 +128,71 @@ void receive(void) {
 }
 
 void transmit(void) {
+  int status;
   
-  int status = loraNode.sendFrame(frameTx, sizeof(frameTx), CONFIRMED, PORT);
+  if(CONFIRMED)   Serial1.print(" Uplink CONFIRMED on PORT ");
+  else            Serial1.print(" Uplink UNCONFIRMED on PORT ");
+  Serial1.println(PORT); 
+  
+#if ((ENABLE_HUMIDITY == false) && (CAYENNE_LPP == false)) 
+    Serial1.print(" Sending Text : ");Serial1.print(frameTx);Serial1.print("\r\n");
+    status = loraNode.sendFrame(frameTx, sizeof(frameTx), CONFIRMED, PORT);
+  
+#elif ((ENABLE_HUMIDITY == true) && (CAYENNE_LPP == false)) 
+    humidityLora = (uint8_t)humidity;
+    Serial1.print(" Sending humidity(%) : ");Serial1.print(humidityLora);Serial1.print("\r\n");
+    status = loraNode.sendFrame((char*)&humidityLora, 1, CONFIRMED);
+
+#else
+    Serial1.println(" Sending all sensor data  with CAYENNE LPP (Low Power Payload) ");
+    status = loraNode.sendFrame((char*)lpp.getBuffer(), lpp.getSize(), CONFIRMED);
+#endif
+
+  
   if(status == LORA_SEND_ERROR) {
     Serial1.println(" Send frame failed!!!");
   } else if(status == LORA_SEND_DELAYED) {
     Serial1.println(" Module is busy : \r\n * It's still trying to send data \r\n OR * \r\n * You are over your allowed duty cycle");
   } else {
-    Serial1.println(" Frame sent. Waiting for Downlink...");
+    Serial1.println(" Waiting for Downlink...");
   }
 }
 
+
+void showConfig(void){
+  Serial1.println("\r\n\r\n\r\n");
+  Serial1.println("#######################################");
+  Serial1.println("#### LoRaWAN Training Session #########");
+  Serial1.println("#####Savoie Mont Blanc University #####\r\n");
+
+  if(ACTIVATION_MODE == OTAA)     {Serial1.println(" * Activation Mode : OTAA");}
+  else                            {Serial1.println(" * Activation Mode : ABP");}
+  
+  if(SEND_BY_PUSH_BUTTON == true) {Serial1.println(" * Send Frame with Push Button");}
+  else                            {Serial1.print(" * Send Frame every ");Serial1.print((FRAME_DELAY<7000)?7000:FRAME_DELAY);Serial1.println("ms");}
+  
+  if(DATA_RATE == 0)              {Serial1.println(" * Data Rate : SF12 BW125");}
+  if(DATA_RATE == 1)              {Serial1.println(" * Data Rate : SF11 BW125");}
+  if(DATA_RATE == 2)              {Serial1.println(" * Data Rate : SF10 BW125");}
+  if(DATA_RATE == 3)              {Serial1.println(" * Data Rate : SF9 BW125");}
+  if(DATA_RATE == 4)              {Serial1.println(" * Data Rate : SF8 BW125");}
+  if(DATA_RATE == 5)              {Serial1.println(" * Data Rate : SF7 BW125");}
+
+  if(ADAPTIVE_DR == true)         {Serial1.println(" * Adaptive Data Rate : ON");}
+  else                            {Serial1.println(" * Adaptive Data Rate : OFF");}
+  
+  if(CONFIRMED == true)           {Serial1.println(" * Frame Confirmed");}
+  else                            {Serial1.println(" * Frame Unconfirmed");}
+
+  Serial1.print(" * Application PORT Number : ");Serial1.print(PORT);Serial1.print("\r\n");
+
+  if      ( ENABLE_HUMIDITY == false && CAYENNE_LPP == false) { Serial1.print(" * Payload is Text : "); Serial1.println(frameTx);}
+  else if ( ENABLE_HUMIDITY == true  && CAYENNE_LPP == false) { Serial1.println(" * Payload is 1 byte : Humidity sensor");}
+  else {  Serial1.println(" * Payload : All Sensors with CAYENNE LPP Format");}
+
+  if(LOW_POWER == true)           {Serial1.println(" * Low Power Mode : ON\r\n");}
+  else                            {Serial1.println(" * Low Power Mode : OFF\r\n");}
+}
 
 void infoAfterActivation(void){
    if(SEND_BY_PUSH_BUTTON == 0){
@@ -102,67 +204,24 @@ void infoAfterActivation(void){
   
 }
 
-/***************************************************************/
-/******************* ABP Function ******************************/
-/***************************************************************/
-#if ACTIVATION_MODE == ABP
-void infoBeforeActivation(void){
-  Serial1.println("\r\n\r\n\r\n");
-  Serial1.println("#######################################");
-  Serial1.println("######## Formation LoRaWAN ############");
-  Serial1.println("######### ABP activation  ##########\r\n");
 
-  // Enable the USI module and set the radio band.
+void infoBeforeActivation(void){
   while(!loraNode.begin(&SerialLora, LORA_BAND_EU_868)) {
     Serial1.println("Lora module not ready");
     delay(1000);
   }
-  loraNode.setDutyCycle(false); 
+  loraNode.setDutyCycle(DISABLE);
+
+ if (ACTIVATION_MODE == ABP) {
   Serial1.print(" * Device Address :          0x ");
   Serial1.println(devAddr);
   Serial1.print(" * Network Session Key :     0x ");
   Serial1.println(nwkSKey);
   Serial1.print(" * Application Session Key : 0x ");
   Serial1.println(appSKey);Serial1.print("\r\n");
-  
- loraNode.setAdaptativeDataRate(DISABLE);
- loraNode.setDataRate(DATA_RATE);
- Serial1.print(" * Data Rate : ");Serial1.print(loraNode.getDataRate());Serial1.print("\r\n");
- if(ADAPTIVE_DR) {
-    loraNode.setAdaptativeDataRate(ENABLE);
-    Serial1.print(" * Adaptive Data Rate : ON");Serial1.println("\r\n");
-  }
-  else {
-    Serial1.print(" * Adaptive Data Rate : OFF");Serial1.println("\r\n");
-  }       
-  loraNode.setDutyCycle(DISABLE);
-}
-
-
-void activation(void){
-  while(!loraNode.joinABP(devAddr, nwkSKey, appSKey)) {
-    Serial1.println(" The Device is not activated on the Server!!");
-    delay(1000);
-  }
-}
-#endif
-
-/***************************************************************/
-/******************* OTAA Function *****************************/
-/***************************************************************/
-
-#if ACTIVATION_MODE == OTAA
-void infoBeforeActivation(void){
-  Serial1.println("\r\n\r\n\r\n");
-  Serial1.println("########################################");
-  Serial1.println("######## LoRaWAN Training Session ######");
-  Serial1.println("#########     OTAA activation   ########\r\n");
-
-  while(!loraNode.begin(&SerialLora, LORA_BAND_EU_868)) {
-    Serial1.println(" Lora module not ready");
-    delay(1000);
-  }
+ }
  
+ if (ACTIVATION_MODE == OTAA){
   str = " * Device EUI :      0x ";
   loraNode.getDevEUI(&str);
   Serial1.println(str);
@@ -170,22 +229,23 @@ void infoBeforeActivation(void){
   Serial1.println(appKey);
   Serial1.print(" * Application EUI : 0x ");
   Serial1.println(appEUI);Serial1.print("\r\n");
-
-  loraNode.setAdaptativeDataRate(DISABLE);
-  loraNode.setDataRate(DATA_RATE);
-  Serial1.print(" * Data Rate : ");Serial1.print(loraNode.getDataRate());Serial1.print("\r\n");
-  if(ADAPTIVE_DR) {
-    loraNode.setAdaptativeDataRate(ENABLE);
-    Serial1.print(" * Adaptive Data Rate : ON");Serial1.println("\r\n");
-  }
-  else {
-    Serial1.print(" * Adaptive Data Rate : OFF");Serial1.println("\r\n");
-  }       
-  loraNode.setDutyCycle(DISABLE);
+ }
+ 
+ loraNode.setAdaptativeDataRate(DISABLE);
+ loraNode.setDataRate(DATA_RATE);
+ if(ADAPTIVE_DR)   loraNode.setAdaptativeDataRate(ENABLE);       
+ loraNode.setDutyCycle(DISABLE);
 }
 
 
-void activation(void){
+void activationABP(void){
+  while(!loraNode.joinABP(devAddr, nwkSKey, appSKey)) {
+    Serial1.println(" The Device is not activated on the Server!!");
+    delay(1000);
+  }
+}
+
+void activationOTAA(void){
   Serial1.println(" JOIN procedure in progress ...");  
   
   while(!loraNode.joinOTAA(appKey, appEUI)) {
@@ -194,4 +254,3 @@ void activation(void){
   }
   Serial1.println(" JOIN procedure : SUCCESS !\r\n");
 }
-#endif
